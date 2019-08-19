97f6428acddc1f49b5d5daa3dfaed4e4586d9b95
EstefaniaGallegos616 <54247200+EstefaniaGallegos616@users.noreply.github.com>
2019-08-18 19:00:12 -0500

    calculator.py

--- calculator.py
+++ calculator.py
@@ -2,15 +2,23 @@
 Your module documentation here
 """
 
-
 class CalculatorClass(object):
-    """
-    Your class documentation here
-    """
-
-    def sum(self, num_list):
-        """
-        Your method documentation here
-        """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+   lista = []
+   num = 0
+   def llenarlista(self):
+       x= int (input("Ingrese Tamaño De La Lista:"))
+       for i in range(x):
+           #add numbers
+           self.lista.append(int(input("Ingrese Numero:")))
+       #print list
+       for i in self.lista:
+           print (i)
+ 
+   def sumar(self):
+       for i in self.lista:
+           self.num+= i
+       print ("La Suma De Los Datos Es:",self.num)
+ 
+obj = CalculatorClass()
+obj.llenarlista()
+obj.sumar()
